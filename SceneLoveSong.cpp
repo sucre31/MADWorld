5d874a5e6fd824f03fb88e31d8d8beddc56a7107
sucre31 <sucre31028@gmail.com>
2024-11-07 03:13:48 +0900

    implement string change system.

--- SceneLoveSong.cpp
+++ SceneLoveSong.cpp
@@ -17,6 +17,8 @@ SceneLoveSong::SceneLoveSong(IOnSceneChangedListener* impl, const Parameter& par
 
 	isMusicPlay = false;
 	currentSoundIndex = 0;
+	strIndex = 1;
+	stringRevive = "＊";
 	Sound::getIns()->loadLoveSongSamples();
 	Image::getIns()->loadLoveSongResources();
 }
@@ -36,27 +38,42 @@ void SceneLoveSong::update()
 	}
 	if (Pad::getIns()->get(ePad::down) == 1) {
 		playSampleSound(0);
+		addReviveChar("ぱ");
 	}
 	if (Pad::getIns()->get(ePad::left) == 1) {
 		playSampleSound(1);
+		addReviveChar("わ");
+		addReviveChar("い");
 	}
 	if (Pad::getIns()->get(ePad::up) == 1) {
 		playSampleSound(2);
+		addReviveChar("ち");
+		addReviveChar("ゅ");
 	}
 	if (Pad::getIns()->get(ePad::right) == 1) {
 		playSampleSound(3);
+		addReviveChar("き");
+		addReviveChar("ゅ");
+		addReviveChar("ん");
 	}
 	if (Pad::getIns()->get(ePad::Y) == 1) {
 		playSampleSound(4);
+		addReviveChar("は");
+		addReviveChar("あ");
+		addReviveChar("ん");
 	}
 	if (Pad::getIns()->get(ePad::B) == 1) {
 		playSampleSound(5);
+		addReviveChar("お");
 	}
 	if (Pad::getIns()->get(ePad::A) == 1) {
 		playSampleSound(6);
+		addReviveChar("が");
 	}
 	if (Pad::getIns()->get(ePad::X) == 1) {
 		playSampleSound(7);
+		addReviveChar("し");
+		addReviveChar("ゃ");
 	}
 	if (Pad::getIns()->get(ePad::start) == 1) {
 		// メニューに戻る
@@ -78,14 +95,64 @@ void SceneLoveSong::playSampleSound(int num) {
 	PlayMovieToGraph(Image::getIns()->getLoveSongMovie()[num]);
 }
 
+/*
+@brief ワイド文字（ひらがな）を受け取って末尾の文字に追加
+*/
+void SceneLoveSong::addReviveChar(std::string mes) {
+	if (strIndex <= strSize - 1) {
+		// ひらがななので2byte分消す 不具合起きる可能性あるかも
+		// あと一文字(2byte)しか引数に取らないようにすべき
+		if (strIndex % 14 == 3 || strIndex % 14 == 8) {
+			stringRevive = stringRevive.substr(0, (strIndex - 1) * 2) + mes + "　　＊";
+			strIndex+=3;
+		}
+		else if (strIndex == 70) {
+			stringRevive = stringRevive.substr(0, (strIndex - 1) * 2) + mes + "　＊";
+			strIndex += 2;
+		}
+		else if (strIndex == 74) {
+			stringRevive = stringRevive.substr(0, (strIndex - 1) * 2) + mes;
+			strIndex += 2;
+		}
+		else {
+			stringRevive = stringRevive.substr(0, (strIndex - 1) * 2) + mes + "＊";
+			strIndex++;
+		}
+	}
+	else {
+		stringRevive.erase(0);
+		strIndex = 1;
+		stringRevive = stringRevive.substr(0, (strIndex - 1) * 2) + mes + "＊";
+		strIndex++;
+	}
+}
+
 void SceneLoveSong::draw() const
 {
 	DrawGraph(0, 0, Image::getIns()->getLoveSongBack(), FALSE);
 	DrawGraph(400, 400, Image::getIns()->getLoveSongMovie()[currentSoundIndex], FALSE);
 	DrawString(400, 30, "ふっかつのじゅもんを いれてください", GetColor(255, 255, 255));
-	DrawString(454, 81, "ぱっが　　っがっ　　がっがが", GetColor(255, 255, 255));
-	DrawString(454, 132, "ががが　　ががが　　はがっは", GetColor(255, 255, 255));
-	DrawString(454, 183, "きゅき　　ゅんき　　ゅはおお", GetColor(255, 255, 255));
-	DrawString(454, 234, "おおお　　はきゅ　　んきゅき", GetColor(255, 255, 255));
-	DrawString(454, 285, "ゅちゅ　　きゅん　　はん＊", GetColor(255, 255, 255));
+
+	// じゅもん描画
+	int strLength = strIndex*2;
+	for (int i = 0; i < 5; i++) {
+		if (i == 4) {
+			if (strLength > i * 28) {
+				DrawString(454, 81 + 51 * i, stringRevive.substr(i * 28, 36).c_str(), GetColor(255, 255, 255));
+				if (strLength < (i + 1) * 28) {
+					// 文字が入っている最後の行
+					break;
+				}
+			}
+		}
+		else {
+			if (strLength > i * 28) {
+				DrawString(454, 81 + 51 * i, stringRevive.substr(i * 28, 28).c_str(), GetColor(255, 255, 255));
+				if (strLength < (i + 1) * 28) {
+					// 文字が入っている最後の行
+					break;
+				}
+			}
+		}
+	}
 }
--- SceneLoveSong.h
+++ SceneLoveSong.h
@@ -7,9 +7,13 @@ public:
     virtual ~SceneLoveSong() = default;
     void update() override;
     void draw() const override;
+    void addReviveChar(std::string);
 private:
+    const int strSize = 74;
     int currentSoundIndex;
     void playSampleSound(int);
     bool isMusicPlay;
+    std::string stringRevive;
+    int strIndex;
 };
 
